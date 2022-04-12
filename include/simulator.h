068788c63cfe1e95c9f9c36d76398142e9ad3b4e
Clark Lu <risheng2@illinois.edu>
2022-04-12 17:09:05 -0500

    set up simulator.h, simulator.cpp, render window

---
+++ include/simulator.h
@@ -0,0 +1,22 @@
+//
+// Created by clark on 2022/4/12.
+//
+#pragma once
+
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+namespace minimetro {
+
+class MiniMetroApp : public ci::app::App {
+public:
+    MiniMetroApp();
+
+    const int kWindowSize = 875;
+
+private:
+
+};
+
+}
---
+++ src/simulator.cpp
@@ -0,0 +1,12 @@
+//
+// Created by clark on 2022/4/12.
+//
+#include "../include/simulator.h"
+
+namespace minimetro {
+
+MiniMetroApp::MiniMetroApp() {
+    ci::app::setWindowSize(kWindowSize, kWindowSize);
+}
+
+}
